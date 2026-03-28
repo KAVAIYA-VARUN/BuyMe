6af3366c741e92310999ec69f27634525f2c32ae
Kavaiya Varun <kavaiyavarun1@gmail.com>
2026-03-28 13:50:37 +0530

    Add production origin to allowedOrigins

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ import reviewRouter from "./Routes/reviewRoutes.js";
 // App config
 const app = express();
 const PORT = process.env.PORT || 4001;
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174', 'http://locahost:5175'];
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174', 'http://locahost:5175', 'https://ecommerce-buy-me.onrender.com'];
 
 // Middlewares
 app.use(express.json());
